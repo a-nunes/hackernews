96e7248104a25520dcf902916fbe2af9f95e0b27
a-nunes <art.nunes@gmail.com>
2019-02-04 03:14:31 -0200

    Iniciando os estudos em React.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const welcomeMsg = 'Welcome to the Road to Learn React.';
+    const user = {
+      firstName: 'Artur',
+      lastName: 'Nunes'
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>{welcomeMsg}</h2>
+        <h4>Be Our Guest {user.firstName} {user.lastName}!</h4>
       </div>
     );
   }
