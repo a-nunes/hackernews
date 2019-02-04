034bcccedf63824b2028a3ef24f8fd1242a660a2
a-nunes <art.nunes@gmail.com>
2019-02-04 18:16:15 -0200

    Fluxo Unidirecional de Dados

--- src/App.js
+++ src/App.js
@@ -21,18 +21,83 @@ const list = [
   },
 ];
 
+const users = [
+  {
+    firstName: 'Artur',
+    lastName: 'Nunes',
+    objectID: 0,
+  },
+  {
+    firstName: 'Paola',
+    lastName: 'Nunes',
+    objectID: 1,
+  },
+  {
+    firstName: 'Igor',
+    lastName: 'Nunes',
+    objectID: 2,
+  },
+  {
+    firstName: 'Mayara',
+    lastName: 'Silva',
+    objectID: 3,
+  },
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      users,
+    };
+    this.onDismissList = this.onDismissList.bind(this);
+    this.onDismissUser = this.onDismissUser.bind(this);
+  }
+
+  onDismissList(id){
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  onDismissUser(id){
+    const isNotId = user => user.objectID !== id;
+    const updatedUsers = this.state.users.filter(isNotId);
+    this.setState({ users: updatedUsers });
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
           <div key={item.objectID}>
             <span>
-              <a href={item.url}>{item.title}</a>
+              <a href={item.url}>{item.title} </a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
+            <span>{item.author} </span>
+            <span>{item.num_comments} </span>
+            <span>{item.points} </span>
+            <span>
+              <button
+                onClick={() => this.onDismissList(item.objectID)}
+                type="button">
+                  Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+        {this.state.users.map((user) =>
+          <div key={user.objectID}>
+            <span>{user.firstName} </span>
+            <span>{user.lastName}</span>
+            <span>
+              <button
+                onClick={() => this.onDismissUser(user.objectID)}
+                type="button">
+                  Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
