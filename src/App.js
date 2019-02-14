2df250e6ad097f9833f1bc4c67e32544a2e54308
a-nunes <art.nunes@gmail.com>
2019-02-14 12:22:04 -0200

    Obtendo Dados

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
 
-const users = [
-  {
-    firstName: 'Artur',
-    lastName: 'Nunes',
-    objectID: 0,
-  },
-  {
-    firstName: 'Paola',
-    lastName: 'Nunes',
-    objectID: 1,
-  },
-  {
-    firstName: 'Igor',
-    lastName: 'Nunes',
-    objectID: 2,
-  },
-  {
-    firstName: 'Mayara',
-    lastName: 'Silva',
-    objectID: 3,
-  },
-];
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 const isSearched = searchTerm =>
   item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
@@ -51,13 +16,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list,
-      users,
-      searchTerm: "",
+      result: null,
+      searchTerm: DEFAULT_QUERY,
     };
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
-    this.onDismissUser = this.onDismissUser.bind(this);
+  }
+
+  setSearchTopStories(result){
+    this.setState ({ result });
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
   }
 
   onDismiss(id){
@@ -66,18 +44,15 @@ class App extends Component {
     this.setState({ list: updatedList });
   }
 
-  onDismissUser(id){
-    const isNotId = user => user.objectID !== id;
-    const updatedUsers = this.state.users.filter(isNotId);
-    this.setState({ users: updatedUsers });
-  }
-
   onSearchChange(event) {
     this.setState({ searchTerm: event.target.value });
   }
 
   render() {
-    const { searchTerm, list, users } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -89,7 +64,7 @@ class App extends Component {
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
